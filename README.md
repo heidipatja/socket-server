9b35fc2ce9a6790ce0734c61b487511cf27c3de3
Heidi Patja <hpatja@gmail.com>
2020-10-02 22:12:23 +0200

    First version of socket server

---
+++ README.md
@@ -0,0 +1,13 @@
+# Socket.io server
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm install`
+
+Install all dependencies.
+
+### `npm start`
+
+Runs the app.
---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+function getTimeStamp() {
+    let now = new Date();
+    now = now.toLocaleTimeString("sv-SE", { hour12: false });
+    return now;
+}
+
+io.origins(['https://socket-server.heidipatja.me:443']);
+
+io.on("connection", function (socket) {
+
+    let activeUser = false;
+
+    socket.on("join", (username) => {
+        if (activeUser) {
+            return;
+        } else {
+            socket.username = username;
+            activeUser = true;
+            io.emit("message", { timestamp: getTimeStamp(), message: `${username} har anslutit till chatten.`})
+        }
+    });
+
+    socket.on("message", function (message) {
+        socket.broadcast.emit("message", message);
+    });
+
+    socket.on("disconnect", function() {
+        io.emit("message", { timestamp: getTimeStamp(), message: `${socket.username} har lämnat chatten.`})
+    });
+});
+
+server.listen(8300);
+
+console.log('Socket server is running on port 8300')
